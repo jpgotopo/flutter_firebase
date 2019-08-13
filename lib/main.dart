a6c12a5af713ac348d98eb802f44cd6a60d920a3
jpgotopo <jpgotopo@gmail.com>
2019-08-13 05:04:00 -0500

    Creando repositorio de Prueba de Flutter + Firebase

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/ui/listview_product.dart';
+void main() => runApp(new MaterialApp(
+  home: ListViewProduct(),
+  title: 'Product Db',
+));
+
